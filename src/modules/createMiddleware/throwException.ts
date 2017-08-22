9408d9739fd81a9f47e0fecdb5118b32acf358b9
1ven <1ven.mironchik@gmail.com>
2017-08-22 16:13:15 +0300

    Fix typo

--- src/modules/createMiddleware/throwException.ts
+++ src/modules/createMiddleware/throwException.ts
@@ -8,7 +8,7 @@ import { ApiEntry } from "../createApiEntry";
 export default (entry: ApiEntry) => {
   throw new Error(
     `Apis are having entries with the same path - ${entry.constants.request.replace(
-      /\/failure/,
+      /\/request/,
       ""
     )}`
   );
