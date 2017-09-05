70d900dd9dd01923bc39172bd1953935828e9797
1ven <1ven.mironchik@gmail.com>
2017-09-05 16:37:55 +0300

    Remove extra function

--- src/helpers/select.ts
+++ src/helpers/select.ts
@@ -1,5 +1,5 @@
 import { compose, prop } from "ramda";
 import { ApiEntry } from "../modules/createApiEntry";
 
-export default (entry: ApiEntry, key: string) => (state, props) =>
+export default (entry: ApiEntry, key: string) =>
   compose(prop(key), entry.selector);
