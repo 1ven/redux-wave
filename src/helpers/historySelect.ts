ff579e36c1a91174ea0021a7acc01acfa5af13f3
1ven <1ven.mironchik@gmail.com>
2017-09-06 09:46:07 +0300

    Make condition param required by default

--- src/helpers/historySelect.ts
+++ src/helpers/historySelect.ts
@@ -3,18 +3,13 @@ import { ApiEntry } from "../modules/createApiEntry";
 import { hasIntersection } from "../utils";
 
 // TODO: make condition requred param
-export default (entry: ApiEntry, prop: string, condition?) => (
-  state,
-  props
-) => {
+export default (entry: ApiEntry, prop: string, condition) => (state, props) => {
   const list = entry.selector(state, props);
-  const item = !condition
-    ? last(list)
-    : list.find(
-        item =>
-          typeof condition(props) === "function"
-            ? condition(props)(item.request)
-            : hasIntersection(condition(props), item.request)
-      );
+  const item = list.find(
+    item =>
+      typeof condition(props) === "function"
+        ? condition(props)(item.request)
+        : hasIntersection(condition(props), item.request)
+  );
   return item && item[prop];
 };
